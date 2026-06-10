3658752f1e2bb2314db9c581c3aee1eadcef5586
ragapay79 <ragapay@liceo.edu.ph>
2026-06-10 15:08:44 +0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,8 @@
+# demo.py local code
+data = [1, 2, 3, 4, 5]
+
+square = lambda x: x * x
+is_even = lambda x: x % 2 == 0
+
+result = list(map(square, filter(is_even, data)))
+print("Squared even numbers:", result)
