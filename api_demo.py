ab7063dc460e5896a33802ca62fa1d7d191c0b4d
ragapay79 <ragapay@liceo.edu.ph>
2026-07-05 09:40:50 +0800

    Add API demo program

---
+++ api_demo.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Post Title:", data["title"])
+else:
+    print("Error:", response.status_code)
